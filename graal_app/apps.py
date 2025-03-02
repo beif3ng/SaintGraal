b50f67d857c66e12e79ab807570ceac57addcd45
Nezdeshniy <69723927+beif3ng@users.noreply.github.com>
2025-03-02 21:52:16 +0600

    init

---
+++ graal_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GraalAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'graal_app'
---
+++ main/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for saint_graal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saint_graal.settings')
+
+application = get_wsgi_application()
