6bf0228728dab31eeaa5d5bd423b1a5afaad216d
Nezdeshniy <69723927+beif3ng@users.noreply.github.com>
2025-03-03 11:20:57 +0600

    Add Category, Transaction models

--- graal_app/admin.py
+++ graal_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Transaction
+
+admin.site.register(Category)
+admin.site.register(Transaction)
--- graal_app/models.py
+++ graal_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Transaction(models.Model):
+    INCOME = 'income'
+    EXPENSE = 'expense'
+    TYPE_CHOICES = [
+        (INCOME, 'Income'),
+        (EXPENSE, 'Expense'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        null=True,
+        related_name='transactions'
+    )
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default=EXPENSE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.get_type_display()} - {self.amount} ({self.category})"
